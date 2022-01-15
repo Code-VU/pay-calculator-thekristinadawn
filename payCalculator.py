380d62e324757ff503adb5b2c86c871319c8ad24
Kristina Palmer <Kristina.Palmer@vu.com>
2022-01-15 15:05:40 -0600

    35

--- payCalculator.py
+++ payCalculator.py
@@ -4,7 +4,7 @@ def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
     rate = input("Enter Rate:")
-    calculatepay= int(hrs)*float(rate)
+    calculatepay= float(hrs)*float(rate)
     print(calculatepay)
     
     # end assignment
