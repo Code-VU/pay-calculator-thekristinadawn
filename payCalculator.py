710d41ad6d135aa47519f41088b06f671875b685
Kristina Palmer <Kristina.Palmer@vu.com>
2022-01-15 16:23:46 -0600

    Other Attempt

--- payCalculator.py
+++ payCalculator.py
@@ -3,6 +3,7 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
 
+    hrs = input("Enter Hours:")
     rate = input("Enter Rate:")
     pay = int(hrs)*float(rate)
     print("Enter Hours:" +" "+ hrs)
